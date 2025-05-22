619a2a717f33274c41c88cef71d687ee6727642e
Mika7596 <mika7596@gmail.com>
2025-05-22 12:21:44 +0200

    Installed Bootstrap and added styles to Main Navbar

--- src/components/main-navbar/MainNavbar.jsx
+++ src/components/main-navbar/MainNavbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './main-navbar.css'
 
 function MainNavbar() {
   return (
-    <nav>
-        <ul>
-            <li><Link to="/home">Home</Link></li>
-            <li><Link to="/StarWars">Star Wars Movies</Link></li>
-            <li><Link to="/all-movies">All Movies</Link></li>
+    <nav className="nav">
+        <ul className='main-navbar-ul'>
+            <li className='nav-link'><Link to="/home">Home</Link></li>
+            <li className='nav-link'><Link to="/StarWars">Star Wars Movies</Link></li>
+            <li className='nav-link'><Link to="/all-movies">All Movies</Link></li>
         </ul>
     </nav>
   )
