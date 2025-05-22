ad6c4b4556ba23a0b5b56ebf8ea65d2c023622d1
Mika7596 <mika7596@gmail.com>
2025-05-22 17:12:39 +0200

    Added a component to display SW movies.

---
+++ src/components/sw-main/SWMain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SWMain() {
+  return (
+    <div>SWMain</div>
+  )
+}
+
+export default SWMain
--- src/pages/StarWars.jsx
+++ src/pages/StarWars.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SWHeader from '../components/sw-header/SWHeader'
 import MainNavbar from '../components/main-navbar/MainNavbar'
+import SWMain from '../components/sw-main/SWMain'
 
 function StarWars() {
   return (
     <>
     <SWHeader/>
     <MainNavbar/>
+    <SWMain/>
     </>
   )
 }
