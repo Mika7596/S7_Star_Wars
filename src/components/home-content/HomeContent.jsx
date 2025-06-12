35d78e1c1f398cd6021bf23f7ed83d82d7acf266
Mika7596 <mika7596@gmail.com>
2025-06-12 10:52:39 +0200

    Changed some styles in Home page

--- src/components/home-content/HomeContent.jsx
+++ src/components/home-content/HomeContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomeContent() {
   return (
-    <div style={{textAlign:"center", padding:"40px"}}>Make sure you log in to find the data you need.</div>
+    <div style={{textAlign:"center", padding:"40px", backgroundImage:"url(src/assets/transparent47.png)", height: "50vh", fontWeight: "bolder", fontSize: "2rem"}}>Make sure you log in to find the data you need.</div>
   )
 }
 
